d92eb7a805795469197bd7ea5582a8c4e01ac31e
Joe Horyza <joseph.mn27@yahoo.com>
2021-04-13 11:03:54 -0500

    Added start of Game handling

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,18 @@
 import '../css/Game.css'
+import Button from 'react-bootstrap/Button'
 
 function Game() {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className="Game">
+                <div className="container-fluid">
+                    <div className="row">
+                        <h2>Blackjack</h2>
+                        <Button type="button" class="btn btn-primary">Play Game</Button>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
