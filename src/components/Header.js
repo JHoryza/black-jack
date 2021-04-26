b5ad39641faf955cb3e8fb08c63064eb1df9818c
Joe Horyza <joseph.mn27@yahoo.com>
2021-04-26 11:39:19 -0500

    Rebranding

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import '../css/Header.css';
 function Header() {
     return (
         <div id="header">
-            <h1>BlackJack</h1>
+            <h1><span style={{color: "black", fontWeight: "bolder"}}>BLACK</span><span style={{color: "red", fontWeight: "bolder"}}>JACK</span></h1>
         </div>
     );
 }
